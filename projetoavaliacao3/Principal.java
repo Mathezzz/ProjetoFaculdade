c2ef4d0d88f9ccbe3aaf46a30b194da71144a968
Mathezzz <andersonmmelo23@outlook.com>
2022-11-23 00:06:33 -0300

    Atualização do projeto

--- projetoavaliacao3/Principal.java
+++ projetoavaliacao3/Principal.java
@@ -8,7 +8,13 @@ public class Principal {
 
     public static void main(String[] args) {
         while (true){
-            Menu.menu();
+            //Menu.menu();
+            ListaNomes lista = new ListaNomes();
+            lista.adiciona("Anderson");
+            lista.adiciona("Laura");
+            lista.adiciona("Vinicius");
+            System.out.println(lista);
+            //Consertar to string na classe ListaNomes.
         }
     }
 }
